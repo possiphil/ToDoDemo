9278f9cb2acefd64a04021c52452113deedd2a93
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-23 12:14:31 +0100

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Philipp Sanktjohanser on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
